5f3919575cd1165fd59e91a411fc6ed433e047cb
Joey Nguyen <jnn193@gmail.com>
2016-01-23 14:18:41 -0600

    Created SearchBar component and imported it into index.js.

--- src/api-key.js
+++ src/api-key.js
@@ -1,1 +1,3 @@
-module.exports = '';
+const ApiKey = '';
+
+export default ApiKey;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import ApiKey from './api-key.js';
+import SearchBar from './components/SearchBar.js';
 
 const API_KEY = ApiKey;
 
 const App = () => {
-    return <div>Hello there</div>
+    return <div>
+        <SearchBar />
+    </div>
 };
 
 ReactDOM.render(
